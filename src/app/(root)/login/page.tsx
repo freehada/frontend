a6a941fc1af9795c019c6dbcce21828dd637af39
kimjaewoo2233 <79129475+kimjaewoo2233@users.noreply.github.com>
2024-08-28 22:55:31 +0900

    config: init

---
+++ src/app/(root)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginOAuthForm from "@/components/login-oauth-form";
+
+
+export default function LoginPage() {
+    return (
+        <div className="h-full w-full flex items-center justify-center">
+            <LoginOAuthForm/>
+        </div>
+    )
+}
